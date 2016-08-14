7e50ef8607161a92ca95a827b7b3831d7788502d
yuu <yuu@yuuk-pc>
2016-08-14 18:34:45 +0900

    板の描画

--- breakout/src/controller/BreakoutPlayFieldController.java
+++ breakout/src/controller/BreakoutPlayFieldController.java
@@ -3,9 +3,12 @@ package controller;
 import java.net.URL;
 import java.util.ResourceBundle;
 import core.Ball;
+import core.Ita;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 
 /**
@@ -18,26 +21,37 @@ public class BreakoutPlayFieldController implements Initializable {
 	private VBox vBox;
 	@FXML
 	private BorderPane ballPane;
+	@FXML
+	private Pane itaPane;
+	
+	@FXML
+	private AnchorPane mainPane;
 	private Ball ball = new Ball();
+	private Ita ita = new Ita();
 
 	@Override
 	public void initialize(final URL location, final ResourceBundle resources) {
-		this.ball.initialize("up", 5, 5);
+		this.initBall();
+		this.initIta();
 		
 	}
-
 	/***
-	 * 描画テスト
-	 * 
-	 * @param root
-	 * 
-	 * @param root
+	 * 板初期描画
 	 */
-	public void ballView() {
+	private void initBall(){
+		this.ball.initialize("up", 5, 5);
 		this.ballPane.setCenter(this.ball.getBall());
 		this.ballPane.setLayoutX(this.ball.getWidth());
 		this.ballPane.setLayoutY(this.ball.getHeight());
-
 	}
+	/***
+	 * 板初期描画
+	 */
+	private void initIta(){
+		this.ita.initialize();
+		this.itaPane.getChildren().add(this.ita.getIta());
+	}
+	
+
 
 }
--- breakout/src/core/BreakoutGameField.java
+++ breakout/src/core/BreakoutGameField.java
@@ -28,12 +28,12 @@ public class BreakoutGameField extends Application {
 		/**
 		 * 画面の初期設定
 		 */
-		Parent root = initTetris();
+		Parent root = initBreakout();
 		/**
 		 * 画面表示処理
 		 */
 		breakoutShow(primaryStage, root);
-		this.breakoutPlayFieldController.ballView();
+
 
 
 	}
@@ -51,7 +51,7 @@ public class BreakoutGameField extends Application {
 	/**
 	 * 初期作成
 	 */
-	public Parent initTetris() throws IOException {
+	public Parent initBreakout() throws IOException {
 
 		URL localtion = getClass().getClassLoader().getResource(
 				"resources/GameField.fxml");
