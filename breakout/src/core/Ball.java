42f121f62a8f38e2a4e211bd7732d16d6b448cc2
yuu <yuu@yuuk-pc>
2016-08-29 02:39:46 +0900

    板の当たり判定の修正

--- breakout/src/core/Ball.java
+++ breakout/src/core/Ball.java
@@ -11,7 +11,7 @@ import javafx.scene.shape.Circle;
 public class Ball {
 	private int height = 250;
 	private int width = 50;
-	private boolean vector=true;
+	private boolean vector=false;
 	private int radius;
 	private int speed;
 	private Circle ball;
@@ -48,7 +48,7 @@ public class Ball {
 	
 	public void changeXY(final int y){
 		this.ball.setCenterY(this.ball.getCenterY()+y);
-		System.out.println(this.ball.getCenterY());
+		
 	}
 
 	public Circle getBall() {
